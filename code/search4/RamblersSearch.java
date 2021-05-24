568e65a6662104323afd55d84eeb2ace8a104700
KaiZheng0101 <email@email.com>
2021-05-25 01:20:13 +0800

    add TerrainMap.java

--- code/search4/RamblersSearch.java
+++ code/search4/RamblersSearch.java
@@ -1,7 +1,29 @@
 /**
- * @author: Kai Zheng
- * @date: 2021-05-19 18:09
- * @Version 1.8
- **/
-public class RamblersSearch {
+ * MapSearch.java
+ * Phil Green 2013 version
+ * Heidi Christensen (heidi.christensen@sheffield.ac.uk) 2021 version
+ *
+ * search for map traversal
+ */
+
+import java.util.*;
+
+public class RamblersSearch extends Search4 {
+
+    private TerrainMap map; // map we're searching
+    private String goal; // goal city
+
+    public TerrainMap getMap() {
+        return map;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public RamblersSearch(TerrainMap m, String g) {
+        map = m;
+        goal = g;
+    }
+
 }
