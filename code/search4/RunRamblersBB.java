b547cee5bc49528f689bb0e03aa4d9116b721978
KaiZheng0101 <email@email.com>
2021-05-25 12:00:30 +0800

    Modify rbb

--- code/search4/RunRamblersBB.java
+++ code/search4/RunRamblersBB.java
@@ -14,45 +14,29 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class RunRamblersBB {
+        public static void main(String[] args) {
 
-    /**
-     * constructor, given a PGM image Reads a PGM file. The maximum greyscale value
-     * is rescaled to be between 0 and 255.
-     *
-     * @param filename
-     * @return
-     * @throws FileNotFoundException
-     */
+            TerrainMap map1 = new TerrainMap("tmc.pgm");
+            System.out.println("Input a goal coordinate");
+            Scanner sc = new Scanner(System.in);
+            System.out.println(String.format("the y-axis no greater than %d", map1.getDepth()));
+            System.out.println(String.format("the x-axis no greater than %d", map1.getWidth()));
+            System.out.println("Input the y-axis for start: ");
+            int ys = sc.nextInt();
+            System.out.println("Input the x-axis for start: ");
+            int xs = sc.nextInt();
+            System.out.println("Input the y-axis for end: ");
+            int ye = sc.nextInt();
+            System.out.println("Input the x-axis for end: ");
+            int xe = sc.nextInt();
 
-    public static void main(String[] arg) throws NullPointerException{
-
-        public class RunRamblersBB {
-
-            public static void main(String[] arg) {
-
-                TerrainMap map1 = new TerrainMap("tmc.pgm");
-                System.out.println("Input a goal coordinate");
-                Scanner sc = new Scanner(System.in);
-                System.out.println(String.format("the y-axis no greater than %d", map1.getDepth()));
-                System.out.println(String.format("the x-axis no greater than %d", map1.getWidth()));
-                System.out.println("Input the y-axis for start: ");
-                int ys = sc.nextInt();
-                System.out.println("Input the x-axis for start: ");
-                int xs = sc.nextInt();
-                System.out.println("Input the y-axis for end: ");
-                int ye = sc.nextInt();
-                System.out.println("Input the x-axis for end: ");
-                int xe = sc.nextInt();
-
-                RamblersSearch searcher = new RamblersSearch(map1, new Coords(ye, xe));
-                // the estimate cost is no use for BB
-                SearchState4 initState = (SearchState4) new RamblersState(new Coords(ys, xs), 0, 0);
+            RamblersSearch searcher = new RamblersSearch(map1, new Coords(ye, xe));
+            // the estimate cost is no use for BB
+            SearchState4 initState = (SearchState4) new RamblersState(new Coords(ys, xs), 0, 0);
 
 
-                String res_astar = searcher.runSearch(initState, "branchAndBound");
-                System.out.println(res_astar);
+            String res_bb = searcher.runSearch(initState, "branchAndBound");
+            System.out.println(res_bb);
 
-            }
-
+        }
     }
-}
