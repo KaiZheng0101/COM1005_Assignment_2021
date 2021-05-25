2813f84912b0405b8d14979de53994b3bd009727
KaiZheng0101 <email@email.com>
2021-05-25 11:41:35 +0800

    Modify TerrainMap.java

--- code/search4/TerrainMap.java
+++ code/search4/TerrainMap.java
@@ -14,13 +14,12 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
 public class TerrainMap {
-    private int[][] tmap; // the map, array of pixels 像素数组
+    private int[][] tmap; // the map, array of pixels
     private int width; // max x dimension
     private int depth; // max y dimension
     private int height; // max height
@@ -44,12 +43,11 @@ public class TerrainMap {
         return height;
     };
 
-    private static final int MAXVAL = 255; // We'll use a constant maximum greyscale value（我们将使用一个常量最大灰度值）
+    private static final int MAXVAL = 255; // We'll use a constant maximum greyscale value
 
     /**
      * constructor, given a PGM image Reads a PGM file. The maximum greyscale value
      * is rescaled to be between 0 and 255.
-     * 读取一个PGM文件。最大灰度值被重新调整为0到255之间。
      *
      * @param filename
      * @return
@@ -62,7 +60,7 @@ public class TerrainMap {
             if (!scanner.hasNextInt()) {
                 irfan = scanner.nextLine();
             }
-            ; // advance until next Int （前进到下一个Int）
+            ; // advance until next Int
             if (!scanner.hasNextInt()) {
                 irfan = scanner.nextLine();
             }
@@ -89,7 +87,6 @@ public class TerrainMap {
     }
 
     // write a terrain map to a file
-    // 将地形图写入文件
     public void writeTMap(String fname) {
 
         try {
@@ -111,12 +108,35 @@ public class TerrainMap {
         }
 
     }
-    // show a path on a terrain map 显示地形地图上的路径
-    // path is ArrayList of Coords 路径是坐标的数组列表
-    // make every pixel on path 255 设置路径255上的每个像素
+    // show a path on a terrain map
+    // path is ArrayList of Coords
+    // make every pixel on path 255
 
     public void showPath(ArrayList<Coords> path) {
         for (Coords c : path)
             tmap[c.gety()][c.getx()] = 255;
+    }
+
+    public int localCost(Coords c1, Coords c2) {
+        int h1 = tmap[c1.gety()][c1.getx()];
+        int h2 = tmap[c2.gety()][c2.getx()];
+        if (h2 <= h1) {
+            return 1;
+        }
+        return 1 + h2 -h1;
+    }
+
+    public double estBetween(Coords c1, Coords c2, String heuristics) {
+        if (heuristics.equals("Manhattan")) {
+            return Math.abs(c2.getx()-c1.getx()) +  Math.abs(c2.gety()-c1.gety());
+        } else if(heuristics.equals("Euclidean")) {
+            double ans = (c2.getx()-c1.getx()) * (c2.getx()-c1.getx());
+            ans += (c2.gety()-c1.gety()) * (c2.gety()-c1.gety());
+            return Math.sqrt(ans);
+        } else if(heuristics.equals("height")) {
+            return localCost(c1,c2)-1;
+        }
+        // else we use mix heuristics
+        return 0.4*estBetween(c1,c2, "Manhattan") + 0.3*estBetween(c1,c2, "Euclidean") + 0.3*estBetween(c1,c2, "height");
     }
 }
