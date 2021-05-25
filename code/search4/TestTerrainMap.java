6a9095ad1029dad3cecc0dd94a8352af0bbd36c7
KaiZheng0101 <email@email.com>
2021-05-25 11:50:29 +0800

    add

---
+++ code/search4/TestTerrainMap.java
@@ -0,0 +1,32 @@
+/**
+ * @author: Kai Zheng
+ * @date: 2021-05-25 11:49
+ * @Version 1.8
+ **/
+
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TestTerrainMap {
+
+    /**
+     * constructor, given a PGM image Reads a PGM file. The maximum greyscale value
+     * is rescaled to be between 0 and 255.
+     *
+     * @param filename
+     * @return
+     * @throws FileNotFoundException
+     */
+
+    public static void main(String[] arg) {
+
+        TerrainMap tm = new TerrainMap("tmc.pgm");
+
+        System.out.println(tm.getWidth());
+        System.out.println(tm.getTmap()[7][2]);
+
+    }
+}
