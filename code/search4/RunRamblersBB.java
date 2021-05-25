226ff192fd0c731091a346f8837677a4035e4489
KaiZheng0101 <email@email.com>
2021-05-25 12:28:45 +0800

    To resubmit

--- code/search4/RunRamblersBB.java
+++ code/search4/RunRamblersBB.java
@@ -14,29 +14,30 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class RunRamblersBB {
-        public static void main(String[] args) {
 
-            TerrainMap map1 = new TerrainMap("tmc.pgm");
-            System.out.println("Input a goal coordinate");
-            Scanner sc = new Scanner(System.in);
-            System.out.println(String.format("the y-axis no greater than %d", map1.getDepth()));
-            System.out.println(String.format("the x-axis no greater than %d", map1.getWidth()));
-            System.out.println("Input the y-axis for start: ");
-            int ys = sc.nextInt();
-            System.out.println("Input the x-axis for start: ");
-            int xs = sc.nextInt();
-            System.out.println("Input the y-axis for end: ");
-            int ye = sc.nextInt();
-            System.out.println("Input the x-axis for end: ");
-            int xe = sc.nextInt();
+    public static void main(String[] arg) {
 
-            RamblersSearch searcher = new RamblersSearch(map1, new Coords(ye, xe));
-            // the estimate cost is no use for BB
-            SearchState4 initState = (SearchState4) new RamblersState(new Coords(ys, xs), 0, 0);
+        TerrainMap map1 = new TerrainMap("tmc.pgm");
+        System.out.println("Input a goal coordinate");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(String.format("the y-axis no greater than %d", map1.getDepth()-1));
+        System.out.println(String.format("the x-axis no greater than %d", map1.getWidth()-1));
+        System.out.println("Input the y-axis for start: ");
+        int ys = sc.nextInt();
+        System.out.println("Input the x-axis for start: ");
+        int xs = sc.nextInt();
+        System.out.println("Input the y-axis for end: ");
+        int ye = sc.nextInt();
+        System.out.println("Input the x-axis for end: ");
+        int xe = sc.nextInt();
+
+        RamblersSearch searcher = new RamblersSearch(map1, new Coords(ye, xe));
+        // the estimate cost is no use for BB
+        SearchState4 initState = (SearchState4) new RamblersState(new Coords(ys, xs), 0, 0);
 
 
-            String res_bb = searcher.runSearch(initState, "branchAndBound");
-            System.out.println(res_bb);
+        String res_bb = searcher.runSearch(initState, "branchAndBound");
+        System.out.println(res_bb);
 
-        }
     }
+}
